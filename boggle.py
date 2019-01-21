5c9d3307f9d4311f50cf81b63026fa235658a96c
Andrew Porritt <andrew.porritt@icloud.com>
2019-01-21 22:13:16 +0000

    neighouring positions

--- boggle.py
+++ boggle.py
@@ -10,3 +10,36 @@ def make_grid(width,height):
         for col in range (width)
     }
     
+def neighbours_of_position(coords):
+    """
+    get neighbours
+    """
+    row = coords[0]
+    col = coords[1]
+    # assign each of the neighbours
+    # top left to top right
+    top_left = (row -1, col -1)
+    top_center = (row -1, col)
+    top_right = (row -1, col +1)
+    
+    left = (row, col -1)
+    # coord of selected
+    right = (row, col +1)
+    
+    bottom_left = (row +1, col -1)
+    bottom_center = (row +1, col)
+    bottom_right = (row +1, col +1)
+    
+    return [top_left, top_center, top_right, left, right, bottom_left, bottom_center, bottom_right]
+    
+def all_grid_neighbours(grid):
+    """
+    get all possible neighbours for each position in the grid
+    """
+    neighbours = {}
+    for position in grid:
+        position_neighbours = neighbours_of_position(position)
+        neighbours[position] = [p for p in position_neighbours if p in grid]
+    return neighbours
+    
+        
