ee6a20ac863a6484ea941fd529fb5fbf9fafe4b9
Andrew Porritt <andrew.porritt@icloud.com>
2019-01-23 12:30:55 +0000

    added dictionary

--- test_boggle.py
+++ test_boggle.py
@@ -76,3 +76,25 @@ class TestBoggle(unittest.TestCase):
         self.assertEqual(oneLetterWord, grid [(0, 0)])
         self.assertEqual(twoLetterWord, grid [(0, 0)] + grid [(1, 1)])
         
+    def test_search_grid_for_words(self):
+        """
+        ensure that certain patterns can be found in a path_to_word
+        """
+        grid = {(0, 0) : "A", (0, 1) : "B", (1, 0) : "C", (1, 1) : "D"}
+        twoLetterWord = "AB"
+        threeLetterWord = "ABC"
+        notThereWord = "EEE"
+        dictionary = [twoLetterWord, threeLetterWord, notThereWord]
+        
+        foundWords = boggle.search(grid, dictionary)
+        self.assertTrue(twoLetterWord in foundWords)
+        self.assertTrue(threeLetterWord in foundWords)
+        self.assertTrue(notThereWord not in foundWords)
+        
+    def test_load_dictionary(self):
+        """
+        test that the dict file is >0
+        """
+        dictionary = boggle.get_dictionary("words.txt")
+        self.assertGreater(len(dictionary), 0)
+        
